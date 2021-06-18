a15fa4c05a02d116beb81df3b4750ae2ba5dc5e7
yilang <xyljames@gmail.com>
2021-06-18 19:38:52 -0400

    Data clean up && add default value for predictions

--- src/app/content/PlannerViwer/planner-viwer/planner-viwer.component.ts
+++ src/app/content/PlannerViwer/planner-viwer/planner-viwer.component.ts
@@ -11,73 +11,72 @@ import { FetchServiceService } from 'src/app/service/fetch-service.service';
 export class PlannerViwerComponent implements OnInit {
   originRoutesList:any = []  
   originStopList:any = []
-
   filteredStopList:any= []
   filteredDirections:any = []
   realRoute = ''
- 
+  tempPredics:any =[]
   formValue ={route:'',stop:'',direction:''}
+  prediction = ''
+
   constructor(private fetchService:FetchServiceService ) { }
   ngOnInit(): void {
+    
+  //fetch route list only for "Rapid Transit" or "Commuter Rail"
    this.fetchService.fetchRoutes().subscribe((res:any)=>
    this.originRoutesList = res.data.filter((item:any)=>
-   item.attributes.description =="Rapid Transit" || item.attributes.description=="Commuter Rail" ))
- 
+   item.attributes.description =="Rapid Transit" || item.attributes.description == "Commuter Rail" ))
 
+  // fetch original stop list
    this.fetchService.fetchStops().subscribe((res:any) => this.originStopList = res.data)
   }
 
-   
+  // Intialize form group
   form = new FormGroup({
     route: new FormControl('', Validators.required),
     stop: new FormControl('', Validators.required),
     direction: new FormControl('', Validators.required)
   });
   
-  get f(){
-    return this.form.controls;
-  }
-  showList(){
-    console.log(this.originRoutesList)
-  }
-  
+ 
+  // form submit to get predicted depature time
   submit(){
 
+    // data clean up
     this.formValue =this.form.value
     this.formValue.route = this.formValue.route.substring(this.formValue.route.indexOf(':')+1,this.formValue.route.length)
     this.formValue.stop = this.formValue.stop.substring(0,this.formValue.stop.length)
-
     let stopId = this.formValue.stop.substring(0,this.formValue.stop.indexOf(':'))
-    console.log('id for time',stopId)
-    
-   
-  }
 
+    // pass stop id to fetch predictions
+    this.fetchService.fetchPredictions(stopId).subscribe((res:any)=> {
+      res.data.length=== 0? this.prediction = 'No Predicted Departure Time Found, Please Try Agian':res.data.map((item:any)=> {
+        if( item.attributes.departure_time) {
+                this.prediction = item.attributes.departure_time
+             }
+      })
+    })
+  }
+ 
   selectRoute(e:any){
     let index = e.target.value.indexOf('=>')
-    console.log("e.target.value",e.target.value)
     let realRoute = e.target.value.slice(index+2)
   
-    console.log(realRoute)
+    //get stop list by selected route
     this.selectStopByRoute(realRoute)
 
     let getIndexFromValue = e.target.value.substring(0,1)
+    //get directions list by selected route
     this.selectDirections( getIndexFromValue )
   }
 
   selectStopByRoute(routeValue:any) {
-
-    console.log(routeValue)
-     let a = this.originStopList
-     this.filteredStopList = this.originStopList.filter((stop:any) =>
-     (stop.attributes.name.includes(routeValue)))
-     this.filteredStopList = [...new Set(this.filteredStopList)]
-     console.log('xxx',this.filteredStopList)
- 
+  //remove duplicates for stop list
+     this.filteredStopList = [...new Set( this.originStopList.filter((stop:any) =>
+      (stop.attributes.name.includes(routeValue))))]
   }
+  
   selectDirections( index:number) {
   this.filteredDirections = this.originRoutesList[index].attributes.direction_names
-  console.log(this.filteredDirections)
   }
 
   
--- src/app/service/fetch-service.service.ts
+++ src/app/service/fetch-service.service.ts
@@ -8,8 +8,10 @@ export class FetchServiceService {
 
   routesURL="https://api-v3.mbta.com/routes"
   stopsURL ="https://api-v3.mbta.com/stops"
+  predictionsURL = `https://api-v3.mbta.com/predictions?filter[stop]=`
   constructor(private http:HttpClient){
   }
    fetchRoutes =()=>this.http.get(this.routesURL)
    fetchStops =()=>this.http.get(this.stopsURL)
+   fetchPredictions = (id:string)=>this.http.get(this.predictionsURL+id)
 }
